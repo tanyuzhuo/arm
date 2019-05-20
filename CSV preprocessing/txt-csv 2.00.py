c8b21ee2239056759d9e69141b065a80a303ea1e
Harrison Ankers <h-ankers@outlook.com>
2019-05-20 16:04:10 +0100

    harry check up most my csvs are half working

--- CSV preprocessing/txt-csv 2.00.py
+++ CSV preprocessing/txt-csv 2.00.py
@@ -46,11 +46,17 @@ class test1_:
         #open file
         with open(self.fileName, 'r') as in_file:
             #output list
-            allLines = []
-            throwawayData = []
-            pins = []
+            self.allLines = []
+            self.meta = []
+            self.throwawayData = []
+            self.pinsData = []
+            self.leakageData = []
+            self.vminData = []
 
             pinsOn = 0
+            firstLeakageFlag=0
+            firstVminFlag=0
+            counter=0
             for line_number, line in enumerate(in_file, 1):
 
                 #removes inital and final whitespace
@@ -59,20 +65,47 @@ class test1_:
                 lineTemp = self.sanitize(lineTemp, line_number)
                 #splits on commas
                 lineFinal = lineTemp.split(",")
-
+                if(re.search("\ASITE",lineTemp)):
+                    counter+=1
+                    self.allLines.append(lineFinal)
                 if(not re.search("^$",lineTemp)):
                     #appends to an array and returns it class wide
-                    lines.append(lineFinal)
-                    self.lines=allLines
+                    self.allLines.append(lineFinal)
 
-                    #sets start flag for pins
-                    if(re.search("TSTNum,Pin,Chn",lineTemp) or (pinsOn is not 0)):
-                        if(re.search("TSTNum,Pin,Chn",lineTemp):
-                            pins = 1
-                        pins.append[]
-                    elif(re.search("\Atb_leakage_low,",lineTemp):
+                    #checks if running times
+                    if(counter > 1):
+                        self.meta.append(lineFinal)
+                    else:
+                        #checks if it is a pin data line
+                        if(re.search("\ATSTNum,Pin,Chn",lineTemp) or (len(lineFinal) is 10) or (len(lineFinal) is 11)):
+                            if(not (lineFinal[0]=="TstNum")):
+                                if(lineFinal[9] != "(F)"):
+                                    lineFinal.insert(9,"(P)")
+                            self.pinsData.append(lineFinal)
+                        if(re.search("\Atb_leakage_",lineTemp)):
+                            if(firstLeakageFlag is 0):
+                                pinsOn=0
+                                firstLeakageFlag = 1
+                                tempList = ["Test_Number","Testing_Item","Test","Range"]
+                                self.leakageData.append(tempList)
+
+                            lineFinal.insert(1,re.search("[a-zA-Z0-9_]+(?=_leakage)",lineFinal[1]).group(0))
+                            lineFinal[2]=re.sub("[a-zA-Z0-9_]+(?=_leakage)","",lineFinal[2])
+                            self.leakageData.append(lineFinal)
+                        if(re.search("\A[0-9]+,tb_sc_yd_vmin_shm",lineTemp) or re.search("\Ashmoo_bsmin_vec_stdcell_",lineTemp)):
+                            if(firstVminFlag is 0):
+                                firstVminFlag = 1
+                                tempList = ["Test_Type","Testing_Item","Test_Type","Range","Value"]
+                                self.vminData.append(tempList)
+                            if(re.search("\Ashmoo_bsmin_vec_stdcell_",lineTemp)):
+                                for i in range(len(lineFinal)):
+                                    self.vminData[len(self.vminData)-1].append(lineFinal[i])
+                            else:
+                                self.vminData.append(lineFinal)
+                        else:
+                            self.throwawayData.append(lineFinal)
                 else:
-                    throwawayData.append(lineFinal)
+                    self.throwawayData.append(lineFinal)
 
 
 
@@ -82,9 +115,49 @@ class test1_:
         with open(fileOutput, "w") as out_file:
             #writes csv file from list
             writer = csv.writer(out_file)
-            writer.writerows(self.lines)
+            writer.writerows(self.allLines)
+
+    def genMetaCSV(self, fileOutput="metaOutput.csv"):
+        #opens output file
+        with open(fileOutput, "w") as out_file:
+            #writes csv file from list
+            writer = csv.writer(out_file)
+            writer.writerows(self.meta)
+
+    def genThrowawayCSV(self, fileOutput="ThrowawayOutput.csv"):
+        #opens output file
+        with open(fileOutput, "w") as out_file:
+            #writes csv file from list
+            writer = csv.writer(out_file)
+            writer.writerows(self.throwawayData)
+
+    def genPinsCSV(self, fileOutput="pinsOutput.csv"):
+        #opens output file
+        with open(fileOutput, "w") as out_file:
+            #writes csv file from list
+            writer = csv.writer(out_file)
+            writer.writerows(self.pinsData)
+
+    def genLeakageCSV(self, fileOutput="leakageOutput.csv"):
+        #opens output file
+        with open(fileOutput, "w") as out_file:
+            #writes csv file from list
+            writer = csv.writer(out_file)
+            writer.writerows(self.leakageData)
+
+    def genVminCSV(self, fileOutput="vminOutput.csv"):
+        #opens output file
+        with open(fileOutput, "w") as out_file:
+            #writes csv file from list
+            writer = csv.writer(out_file)
+            writer.writerows(self.vminData)
 
 test1 = test1_()
 test1.input()
 test1.genLines()
 test1.genFullCSV()
+test1.genMetaCSV()
+test1.genThrowawayCSV()
+test1.genPinsCSV()
+test1.genLeakageCSV()
+test1.genVminCSV()
